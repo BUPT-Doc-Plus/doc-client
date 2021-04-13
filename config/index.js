f11527b38e7ee9d1f6427b69ab7325aa3edd3cd4
unknown <>
2021-04-13 16:09:36 +0800

    1

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: info["doc-client"].host, // can be overwritten by process.env.HOST
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
     port: info["doc-client"].port, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
